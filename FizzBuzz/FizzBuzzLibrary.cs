19e541586facf968cea2373c2092e34e5a4ee40d
Ethan Goodwin <23226145+VirusFreeNewt@users.noreply.github.com>
2019-10-06 13:25:02 -0600

    Added The FizzBuzz methods
    git push

---
+++ FizzBuzz/FizzBuzzLibrary.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+
+namespace FizzBuzz
+{
+    class FizzBuzzLibrary
+    {
+        public static void FizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void FizzBuzz(int range)
+        {
+            for (int i = 1; i <= range; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void FizzBuzz(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void FizzBuzz(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+                else if (numbers[i] % 3 == 0 && numbers[i] % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (numbers[i] % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (numbers[i] % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 1, 5, 5, 3, 15, 0 };
+            FizzBuzzLibrary.FizzBuzz(-5, 400);
+            Console.ReadLine();
+        }
+    }
+}
